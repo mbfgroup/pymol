078ed00d86800ece3a1720d8f8959d3254fdca7d
mbfgroup <francisgroupcalendar@gmail.com>
2026-04-22 10:20:05 -0700

    Create pymol_graphics.py

---
+++ pymol_graphics.py
@@ -0,0 +1,14 @@
+# fun commands to play around with for rendering
+bg_color white # so you can get ray tracing in black outline
+set ambient, 0.5 # better PyMol lighting
+set cartoon_color, hydrogen # I like this color for cartoons
+set cartoon_transparency, 0.7
+set ray_trace_disco_factor, 0.3 # can play around with this, 0 = strong, 1 = off
+# disco only has an effect with ray trace values of 1 - 3
+# I like having disco on, you can see kinks/bends in structures better for sticks
+set field_of_view, 70 # can play around with this to change perspective
+ray # renders the ray image
+png filename.png # to save
+#  sometimes the waters mess up the surface rendering, so you need to remove waters before trying to render the protein
+show surface, polymer # to show surface for the protein
+set transparency, 0.7 # 0 is opaque, 1 is fully transparent
